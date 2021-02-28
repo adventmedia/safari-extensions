f3f31d061a557fe6582d4476e22823d002ea2d2e
Andrew Macrae <andrew@adventmedia.net>
2021-02-28 14:53:16 -0500

    converted to xcode

---
+++ reveal password/xproj/Reveal Password/Reveal Password/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Reveal Password
+//
+//  Created by Andrew Macrae on 2021-02-28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
